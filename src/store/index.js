fcd3a9c1d5177196cac67be0111b09cc5b79f5c8
eddyzhang <eddyzhang1986@msn.cn>
2021-05-24 20:18:50 +0800

    持久化因为动态调用route的问题所以不能开

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user';
+import createPersistedState from "vuex-persistedstate";
+
+//持久化user
+// const userPersisted = createPersistedState({
+//   paths: ['user']
+// })
+
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
     user: user
-  }
+  },
+  //plugins: [userPersisted]
 })
 
 export default store;

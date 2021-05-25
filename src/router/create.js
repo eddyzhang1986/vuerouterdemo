e7a3995d14f21d9987a469188622af51b6a1ed8f
eddyzhang <eddyzhang1986@msn.cn>
2021-05-25 11:13:07 +0800

    create

--- src/router/create.js
+++ src/router/create.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter);
 
 
 const createRouter = (routes) => {
+
     const router = new VueRouter({
         mode: 'hash',
         routes: routes
@@ -13,14 +14,36 @@ const createRouter = (routes) => {
 
     //add addroutes methods
     router.$addRoutes = (params) => {
+
+        //清空router的routes数据
         router.matcher = new VueRouter({
             mode: 'hash'
-        }).matcher
-        const rts = [...routes, ...params]
-        for (let r of rts) {
-            router.addRoute(r)
+        }).matcher;
+
+        let routes1 = [...routes];
+        let routes2 = [...params];
+        let routes3 = [];
+
+        for (let rts1 of routes1) {
+            if (rts1.name == '') {
+
+                //替换
+                //删除
+
+            } else {
+                routes3.push(rts1);
+            }
+        }
+
+        //新的routes数组
+        let routes4 = [...routes3, ...routes2]
+
+        //添加新的rts4数据
+        for (let rts4 of routes4) {
+            router.addRoute(rts4)
         }
     }
+
     return router;
 }
 
